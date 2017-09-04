b0838d941e9ad2221314909862925e1bde38bac5
Unknown <csmediaseattle@gmail.com>
2017-09-03 21:35:50 -0700

    Import old code

---
+++ RIFTool/Converter.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mstum.utils;
+
+
+namespace RIFTool
+{
+    public partial class Converter : Form
+    {
+        
+        private Bitmap image;
+        private FileStream outputRIF;
+        private int outPos = 0;
+        public Converter()
+        {
+            InitializeComponent();
+        }
+
+        private void Converter_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void run()
+        {
+
+            prepConvert();
+            
+            
+        }
+
+
+
+        private void prepConvert()
+        {
+            OpenFileDialog inputFileDialog = new OpenFileDialog();
+            string location = "null";
+            inputFileDialog.Title = "Select an image file";
+            inputFileDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
+            if (inputFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                location = inputFileDialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Image not selected! Aborting!", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+
+            }
+            image = new Bitmap(location);
+            SaveFileDialog outputFileDialog = new SaveFileDialog();
+            outputFileDialog.Title = "Choose where to save converted file";
+            outputFileDialog.Filter = "RIF Image(*.RIF)|*.RIF";
+            outputFileDialog.AddExtension = true;
+            if (outputFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                location = outputFileDialog.FileName;
+            }
+            else
+            {
+                MessageBox.Show("Output location not selected! Aborting!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+
+            outputRIF = File.Create(location);
+            convert();
+        }
+
+        private void convert()
+        {
+            
+            ArrayList imageData = new ArrayList();
+            int h = (int) image.Width;
+            
+            int v = (int) image.Height;
+            
+            progressBar1.Maximum = (h * v);
+            progressBar1.Step = 1;
+
+            //outputRIF.AutoFlush = true;
+            
+            writeHelper(Base36.Encode(h));
+            writeHelper("|");
+            writeHelper(Base36.Encode(v));
+            writeHelper("|");
+            outputRIF.Flush();
+            int i;
+            for(i = 0; i < v; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    Color pixel = image.GetPixel(j, i);
+                    if (imageData.Contains(pixel.ToArgb().ToString()))
+                    {
+                        int idx = imageData.IndexOf(pixel.ToArgb().ToString());
+                        writeHelper("&" + Base36.Encode(idx));
+                    }
+                    else
+                    {
+                        imageData.Add(pixel.ToArgb().ToString());
+                        string sub = pixel.ToArgb().ToString();
+                        if (pixel.ToArgb() < 0)
+                        {
+                            sub = sub.Substring(1);
+                            writeHelper(Base36.Encode(int.Parse(sub)));
+                        }
+                        else
+                        {
+                            writeHelper("+" + Base36.Encode(int.Parse(sub)));
+                        }
+
+
+                    }
+                    writeHelper("|");
+                    outputRIF.Flush();
+                    progressBar1.PerformStep();
+                }
+            }
+            outputRIF.Close();
+            MessageBox.Show("RIF sucessfully created.");
+            this.Close();
+        }
+
+        /*
+         * Helper Methods
+         */
+        private void writeHelper(string data)
+        {
+            
+            byte[] toWrite = new ASCIIEncoding().GetBytes(data);
+            outputRIF.Write(toWrite,0,toWrite.Length);
+            //outPos += toWrite.Length-1;
+        }
+
+        private string toHex(int num)
+        {
+            return num.ToString("X");
+        }
+        //To go back to int
+        //return Convert.ToInt32(hex, 16);
+    }
+}
---
+++ RIFTool/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace RIFTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert image to RIF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Convert RIF to image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 107);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(152, 174);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Size Multiplier:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 231);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Railgun Renderer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
